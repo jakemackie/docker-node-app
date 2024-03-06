189db420e0a5ce3f769dcd7350a8d280e244b00d
Jake Mackie <jake.wmackie@gmail.com>
2024-03-06 06:57:12 +0000

    Update index.js
    Removal of unnecessary env variable usage

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ app.get("/", (req, res) =>
   res.json({ response: "Exposed Express API Endpoint" })
 );
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 
 app.listen(port, () =>
   console.log(`app listening on http://localhost:${port}`)
